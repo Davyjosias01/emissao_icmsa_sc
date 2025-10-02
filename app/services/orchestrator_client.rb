4f6ed7cf643cf8f1dce8641c64672f991a3c7b30
Davy Josias Scheuermann <davy.josias@gmail.com>
2025-10-02 08:55:44 -0300

    gerado o Orchestrator_client::get obligations do backoffice

---
+++ app/services/orchestrator_client.rb
@@ -0,0 +1,23 @@
+class OrchestratorClient
+  def initialize(base_url: ORCH_CONFIG[base_url], token: ORCH_CONFIG[token])
+    @base_url = base_url
+    @token = token
+  end
+
+  def get_obligations
+    res = RestClient.get(
+      "#{@base_url}/integration/v1/companies/index", { 
+        params: { 
+          obligation: "emissao_dare", 
+          date_start: "2025-09-01", 
+          date_end: "2025-10-01",
+          integrated_at: "false",
+          obligation_finished: "false"
+        }
+        authorization: @token,
+        accept: :json 
+      }
+    )
+    JSON.parse(res.body)
+  end
+end
--- config/initializers/orchestrator.rb
+++ config/initializers/orchestrator.rb
@@ -1,5 +1,4 @@
 ORCH_CONFIG = {
-  obligations_base_url:   ENV['OBLIGATIONS_URL'],
-  services_base_url:     ENV['SERVICES_URL'],
-  integration_token:  ENV['INTEGRATION_TOKEN'],
+  base_url:   ENV['BASE_URL'],
+  token:  ENV['TOKEN'],
 }.freeze
